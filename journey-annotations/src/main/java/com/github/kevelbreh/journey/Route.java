2b348db5ab52e00043c8b7ba22adbfc0e8e2c094
Kevin Woodland <kiwoodland@gmail.com>
2017-09-04 10:28:33 +0200

    Retention type RUNTIME

--- journey-annotations/src/main/java/com/github/kevelbreh/journey/Route.java
+++ journey-annotations/src/main/java/com/github/kevelbreh/journey/Route.java
@@ -4,9 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Retention(CLASS) @Target(TYPE)
+@Retention(RUNTIME) @Target(TYPE)
 public @interface Route {
   String value() default "";
   String provider() default "default";
